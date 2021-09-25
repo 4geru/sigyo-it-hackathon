fad99227b393e66a1d74db55a6d00048ba51a5d1
kanebest <kane01best@gmail.com>
2021-09-26 00:32:05 +0900

    change card-component

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Card from '@components/Card'
 import Layout from '@components/Layout'
 import { Link } from "gatsby"
 
@@ -6,23 +7,22 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Top">
       <title>旅ネーター</title>
-      <div className="grid place-items-center">
-        <div>
-          誰と一緒に行く？
+      <Card title={'誰と一緒に行く？'}>
+        <div className="grid place-items-center">
+          <div className="search-answer">
+            <Link className="btn btn-wide" to="/search2">恋人</Link>
+          </div>
+          <div className="search-answer">
+            <Link className="btn btn-wide" to="/search2">家族</Link>
+          </div>
+          <div className="search-answer">
+            <Link className="btn btn-wide" to="/search2">友達</Link>
+          </div>
+          <div className="search-answer">
+            <Link className="btn btn-wide" to="/search2">ひとりで</Link>
+          </div>
         </div>
-        <div className="search-answer">
-          <Link className="btn btn-wide" to="/search2">恋人</Link>
-        </div>
-        <div className="search-answer">
-          <Link className="btn btn-wide" to="/search2">家族</Link>
-        </div>
-        <div className="search-answer">
-          <Link className="btn btn-wide" to="/search2">友達</Link>
-        </div>
-        <div className="search-answer">
-          <Link className="btn btn-wide" to="/search2">ひとりで</Link>
-        </div>
-      </div>
+      </Card>
     </Layout>
   )
 }
