2910e80534e4d732bf695019d1afd3e30a8bb679
4geru <westhouse51@gmail.com>
2021-09-25 17:55:37 +0900

    検索結果のデザインの追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => {
       <title>Home Page</title>
       <div className="grid place-items-center">
         <Link className="btn btn-wide" to="/playground">PlayGround</Link>
+        <Link className="btn btn-wide" to="/search_result">検索結果</Link>
       </div>
     </Layout>
   )
